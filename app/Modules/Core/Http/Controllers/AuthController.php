93a4597a48cc0949d670a1f43474da78841a1824
Tariq Alwalidi <tariq.alwalidi@gmail.com>
2026-07-12 23:14:20 +0300

    Remove stale cache import

--- app/Modules/Core/Http/Controllers/AuthController.php
+++ app/Modules/Core/Http/Controllers/AuthController.php
@@ -14,7 +14,6 @@ use App\Modules\Core\Services\TwoFactorService;
 use App\Modules\Shared\Models\ActivityLog;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
